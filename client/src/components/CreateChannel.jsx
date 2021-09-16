6e2dc7f26b4a8ef0ffe989bbd32acdf805f1acae
samacriano <samacriano@gmail.com>
2021-09-16 23:05:22 +0100

    first commit

---
+++ client/src/components/CreateChannel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useChatContext } from "stream-chat-react";
+
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
+
+const CreateChannel = () => {
+  return <div>Create Channel</div>;
+};
+
+export default CreateChannel;
